16d8babbd030f9b34d757c9808fded669a7add91
Izaack Kuria <kuriaisac@gmail.com>
2024-07-10 21:00:33 +0300

    Adds fetch()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+//call the form
+const inputForm = document.querySelector("form");
+//event listener to 
+inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
 
+    console.log(input.value);
+//fetch data from local host
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.textContent = `Title: ${data.title}`;
+            summary.textContent = `Summary: ${data.summary}`;
+    });
+}); 
+}
 document.addEventListener('DOMContentLoaded', init);
+
